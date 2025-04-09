e40323c4dc5b84cf8e4fe54f24447dcf7262dc49
Marycrazy <marianapaisjorgemagalhaes@gmail.com>
2025-04-09 03:49:51 +0100

    Registo API, 1º teste

--- api/index.py
+++ api/index.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import psycopg
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Conexão à BD
+def get_connection():
+    return psycopg.connect(
+        host="localhost",
+        database="BDII proj",
+        user="postgres",
+        password="123456789"
+    )
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/auth/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    nome = data.get("nome")
+    email = data.get("email")
+    password = data.get("password")
+    tipo = data.get("tipo_utilizador")
+
+    # Validação básica
+    if not all([nome, email, password, tipo]):
+        return jsonify({"erro": "Todos os campos são obrigatórios."}), 400
+
+
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        # Chamar a função PL/pgSQL
+        cur.execute("SELECT inserir_utilizador(%s, %s, %s, %s)", (nome, email, password, tipo))
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return jsonify({"mensagem": "Utilizador registado com sucesso."}), 201
+
+    except psycopg.Error as e:
+        conn.rollback()
+        return jsonify({"erro": str(e)}), 400
