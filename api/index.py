0bb9676250094280206a45448f87967ba236db2d
Marycrazy <marianapaisjorgemagalhaes@gmail.com>
2025-05-15 00:29:45 +0100

    Adiciona endpoint de autenticação de utilizador e função correspondente no banco de dados

--- api/index.py
+++ api/index.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 import psycopg, os
-from .auth import register
+from .auth import register, login
 from .reserve import reserva
 from .room import quarto
 
 app = Flask(__name__)
 app.register_blueprint(register)
+app.register_blueprint(login)
 app.register_blueprint(reserva)
 app.register_blueprint(quarto)
 
--- schema/funçoes/funcoes.sql
+++ schema/funçoes/funcoes.sql
@@ -6,3 +6,13 @@ BEGIN
     INSERT INTO utilizadores(NOME, EMAIL, PASSWORD, TIPO_UTILIZADOR, DATA_REGISTO) VALUES (u_nome, u_email, crypt(u_password,  gen_salt('bf')), u_tipo_utilizador::TIPO_UTILIZADOR, CURRENT_DATE);
 END;
 $$ LANGUAGE plpgsql;
+
+--@block
+--Função para autenticar utilizador
+CREATE OR REPLACE FUNCTION autenticar_utilizador (u_email VARCHAR(50), u_password VARCHAR(50))
+RETURNS INTEGER AS $$
+BEGIN
+    RETURN (SELECT ID_UTILIZADORES FROM utilizadores WHERE email = u_email AND password = crypt(u_password,  password));
+END;
+$$ LANGUAGE plpgsql;
+
